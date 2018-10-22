c97bfde066ad5f3072c72603dd7a098c83d38f8e
Administrator <Administrator@USER-20170605KJ>
2018-10-22 15:13:40 +0800

    n1

--- NumberOperator/src/OpMove.java
+++ NumberOperator/src/OpMove.java
@@ -7,7 +7,11 @@ public class OpMove {
 		
 		System.out.print("as"); 
 		//124
+<<<<<<< Upstream, based on branch 'master' of https://github.com/wabinogi/JavaBasicTest.git
 		//from new
+=======
+		//88888888888888888
+>>>>>>> 9a05881 n1
 	}
 
 }
