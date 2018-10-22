1f89fd0d5cd49930474efb1c7c9d659a345a9ccf
Administrator <Administrator@USER-20170605KJ>
2018-10-22 15:56:41 +0800

    1

--- NumberOperator/src/OpMove.java
+++ NumberOperator/src/OpMove.java
@@ -4,7 +4,11 @@ public class OpMove {
 	 
 	public static void main(String[] args)
 	{
-		
+		//GIT合并的方法
+		//1.fetch到本地，千万不要pull，一旦PULL就直接覆盖了
+		//2.本地的代码COMMIT
+		//3.在项目里选择MERGE
+		//4.在.JAVA文件中选择MERGE TOOLS
 		System.out.print("as"); 
 	//88888888888888
 
