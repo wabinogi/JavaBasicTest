38d6694d242910852bbf614e52c0a7ac85742c48
Administrator <Administrator@USER-20170605KJ>
2018-10-29 17:14:13 +0800

    isok

---
+++ BasicArray/src/CollectionTest.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.List;
+
+//如果没有实现Cloneable，调用clone()方法，会抛出CloneNotSupportedException
+public class CollectionTest implements Cloneable {
+
+	public static void main(String... args) throws CloneNotSupportedException
+	{
+		Collection c;
+		List l;
+		ArrayList al;
+		HashMap hm;
+		HashSet ht;
+		Hashtable ha;
+		CollectionTest ct1 = new CollectionTest();
+		
+	}
+	
+	
+}
