861967ce233436add44b5d58405a174040218789
Administrator <Administrator@USER-20170605KJ>
2018-10-30 17:04:23 +0800

    17：04

--- BasicArray/src/CollectionTest.java
+++ BasicArray/src/CollectionTest.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Vector;
 
 //如果没有实现Cloneable，调用clone()方法，会抛出CloneNotSupportedException
 public class CollectionTest implements Cloneable {
@@ -17,6 +18,7 @@ public class CollectionTest implements Cloneable {
 		HashSet ht;
 		Hashtable ha;
 		CollectionTest ct1 = new CollectionTest();
+		Vector vr;
 		
 	}
 	
