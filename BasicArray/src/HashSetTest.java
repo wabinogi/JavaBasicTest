8c87f1123c4515f9aa2525b6891523643a88a3ae
Administrator <Administrator@USER-20170605KJ>
2018-10-31 15:55:48 +0800

    Win 10.31 15:33

---
+++ BasicArray/src/HashSetTest.java
@@ -0,0 +1,27 @@
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class HashSetTest {
+
+	public static void main(String... args) 
+	{
+		
+	}
+	
+	public static void test()
+	{
+		HashSet hs;
+		TreeSet ts;
+	    LinkedHashSet lhs;
+	    HashMap hm;
+	   
+	    TreeMap tm;
+		
+	}
+	
+}
