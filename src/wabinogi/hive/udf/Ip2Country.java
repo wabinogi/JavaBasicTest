df42893ed805facfde046aad9ee262b5f4f8eb5f
Administrator <Administrator@USER-20170605KJ>
2018-11-13 17:13:04 +0800

    123123

--- src/wabinogi/hive/udf/Ip2Country.java
+++ src/wabinogi/hive/udf/Ip2Country.java
@@ -1,7 +1,7 @@
 package wabinogi.hive.udf;
  
 import java.io.File;
-import java.io.IOException;
+import java.io.IOException;  
 import java.net.InetAddress;
 import org.apache.hadoop.hive.ql.exec.UDF;
 import com.maxmind.db.CHMCache;
