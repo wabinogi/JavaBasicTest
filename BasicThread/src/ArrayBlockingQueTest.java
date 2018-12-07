e0436ffc477215bf8b1bab6a84a8427380461a8b
wabinogi <wabinogi@192.168.31.247>
2018-12-07 11:21:28 +0800

    12.7

--- BasicThread/src/ArrayBlockingQueTest.java
+++ BasicThread/src/ArrayBlockingQueTest.java
@@ -1,68 +1,86 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.concurrent.ArrayBlockingQueue;
+//FIFO，且每次添加、删除都会使用重入锁，反复使用添加、删除，会频繁触发锁资源申请和释放，估计效率不高
+//每次添加使用可以抛出InterruptedException的方法，例如PUT和TAKE，这两个方法都会AWAIT
+//POLL和OFFER在遇到队列空或者满了会返回NULL
+//DRAINTO是把ArrayBlockingQueue拷贝到目标队列中
+//如果要把多个原子操作一次加入队列，则先加入一个AL中，然后该AL对象PUT入队列
 
 public class ArrayBlockingQueTest implements Runnable {
 
-	Object1 ob1 = new Object1();
-	static ArrayBlockingQueue<Object1> abq = new ArrayBlockingQueue<Object1>(5);
+	int i = 1;
+	static ArrayBlockingQueue<ArrayList> abq = new ArrayBlockingQueue<ArrayList>(5);
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		ArrayBlockingQueTest ot = new ArrayBlockingQueTest();
-		InputObject io = ot.new InputObject();
+		ArrayBlockingQueTest ot1 = new ArrayBlockingQueTest();
+		ArrayBlockingQueTest ot2 = new ArrayBlockingQueTest();
 
-        //Thread 
-		 Thread t = new Thread(ot);
-		 Thread t2 = new Thread(io);
+        //Thread ，2个生产者
+		 Thread t = new Thread(ot1);
+		 Thread t2 = new Thread(ot2);
 		 t2.start();
 		 t.start();
+
+		 //一个消费者
+		 System.out.println("Consumer: ");
+		 ot1.consumer();
 		
-	
-		 //
-		 ot.ob1.ii = 1;
-		 System.out.println("Que in: " + ot.ob1.ii);
-		 abq.offer(ot.ob1);
 		
 	}
     
+	public void input1() throws InterruptedException 
+	{
+		 String ThreadName = Thread.currentThread().getName();
+		 
+		 int j = 10;
+		 ArrayList<String> al = new ArrayList<String>(10);
+		 String PrintOb;
+		 while(j >=1)
+		 {
+			 j--;
+			 PrintOb = ThreadName + ": " + i; 
+			 i = i + 1;
+			 //多个原子动作封入一个AL中
+			 al.add(PrintOb);
+		 }
+		 System.out.println(ThreadName + " Start !");
+		 abq.put(al);
+	}
+	
 	public void consumer() throws InterruptedException
 	{
-		System.out.println("Que out: ");
-		Object1 oo = abq.poll();
-		while(oo == null) 
-	    {
-			Thread.sleep(500);
-			 oo = abq.poll();
-	    }
-	    
-		while(oo != null)
+		while(true)
 		{
-		  oo.printNo();
-		  oo = abq.poll();
-		}
-		
+		   Thread.sleep(50);
+		   ArrayList al = abq.take();
+		   Iterator<String> it = al.iterator();
+		   while(it.hasNext())
+		   {
+			  
+			   System.out.println(it.next());
+		   }
+		}	
 	}
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
 		try {
-			consumer();
+			input1();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 	}
     
-	public class InputObject implements Runnable
-	{
-		Object1 ob2 = new Object1();
-		@Override
-		public void run() {
-			// TODO Auto-generated method stub
-			 ob2.ii = 2;
-			 System.out.println("Que in: " + ob2.ii);
-			 abq.offer(ob2);
-		}
-		
-	}
+	
+	
+	
+	
+	
+	
+
 	
 }
