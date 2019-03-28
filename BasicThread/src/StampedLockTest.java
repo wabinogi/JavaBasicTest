fb7c9d00264aa87d7a3143e0e2b23bfb13da7981
Administrator <Administrator@USER-20170605KJ>
2019-03-28 17:00:11 +0800

    OK

--- BasicThread/src/StampedLockTest.java
+++ BasicThread/src/StampedLockTest.java
@@ -5,7 +5,19 @@ public class StampedLockTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//ORIGIN 256 100000000
+		
+		//ABITS 255  011111111
+		//WBIT 128   010000000
+		//SBITS -128 110000000
+		//RBITS 127  001111111
+		//RFULL 126  001111110
+		//s+WBIT     110000000
+		
+		
 		StampedLock sl = new StampedLock();
+		
+		System.out.println(Integer.toBinaryString(-128) );
 	}
 
 }
