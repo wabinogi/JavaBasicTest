5d33d8c1d1f3971c0d7bd7f86f14ad1fdd420658
wabinogi <wabinogi@192.168.31.247>
2019-03-31 22:37:37 +0800

    ok

--- BasicThread/src/Singleton.java
+++ BasicThread/src/Singleton.java
@@ -3,33 +3,41 @@ public class Singleton {
 
    public static void main(String...agrs)
    {
-	   Integer i = new Integer(1);
-	   Integer j = new Integer(0);
-	   j = i;
-	   i = 20;
-	   System.out.println(i);
-	   System.out.println(j);
+	 
+	   
    }
 	
    private static Singleton sg;
-   public int i = 0;
-   private Singleton()
+   //锁定用变量
+   private static Integer var = 0;
+   static public Singleton getInstant()
    {
-	   
-   }
-   
-   private Singleton(int ii)
-   {
-	   this.i = ii;
-   }
-   
-   
-   static public Singleton getInstant(int iii)
-   {
-	   if(sg == null)
-	   {
-		   sg = new Singleton(iii);
+	   if(sg == null){
+		  
+		  //如果不锁定var变量，该单例模式是线程不安全的
+		   //有可能多个线程同时判断sg == null，初始化对象
+	      synchronized(var)
+	      {
+	        if(sg == null)
+	        {
+		      sg = new Singleton();
+	        }
+	      }
+	      
 	   }
 	   return sg;
    }
+
+   public static class subSingleton
+   {
+	   //jvm一旦加载subSingleton类，静态实例完成初始化
+	   //不存在多线程不安全问题
+	   //就是理论上加载时会多一步实例化的过程，因此会稍慢？
+	   private static subSingleton ssl = new subSingleton();
+	   public static subSingleton getSubInstance()
+	   {
+		   return ssl;
+	   }
+   }
+
 }
