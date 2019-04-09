d9e486383d888e650b353bc94929f292aa5ecb49
Administrator <Administrator@USER-20170605KJ>
2019-04-09 17:04:03 +0800

    321

---
+++ BasicArray/Note/LinkedTransferQueNote.java
@@ -0,0 +1,45 @@
+import java.util.concurrent.LinkedTransferQueue;
+//无界队列
+//由于是无界队列，因此不存在容量上限，生产者永远不会阻塞
+//FIFO
+//tryTransfer,如果没有消费者，立刻返回，不进入队列等待
+//消费线程进行TAKE操作时，如果队列为空，会生成一个E为NULL的节点，进行预占用，同时自旋一定时间，若仍不能取到，则如队列进行PARK挂起（阻塞）
+//生产单线程多次进行TAKE操作，按多次进入队列算
+
+//考虑到维护头尾指针的开销，以及链表结构维护的开销，采用了基于SLACK值的头尾指针维护算法。不是每次PUT都会更新头尾指针
+//头尾之间的2~3个NODE元素相当于缓冲区
+//基于M$S队列算法变种
+//感觉比SynchronousQueue要高效
+public class LinkedTransferQueNote implements Runnable{
+
+	static Integer signal = 0;
+	static Integer signal1 = 1;
+	static Integer signal2 = 2;
+	static Integer signal3 = 3;
+	static Integer signal4 = 4;
+	static LinkedTransferQueue<Integer> ltq = new LinkedTransferQueue<Integer>();
+	
+	public static void main(String[] args) throws InterruptedException {
+
+
+		LinkedTransferQueNote ltqn  = new LinkedTransferQueNote();
+        Thread t = new Thread(ltqn);
+        t.start();
+        
+        Thread.currentThread().sleep(200);
+        ltq.put(signal);
+        ltq.put(signal1);
+        ltq.put(signal2);
+        ltq.put(signal3);
+        ltq.put(signal4);
+	
+		
+	}
+
+	@Override
+	public void run() {
+	
+		 
+	}
+
+}
--- BasicArray/Note/SynchronousQueNote.java
+++ BasicArray/Note/SynchronousQueNote.java
@@ -4,13 +4,19 @@ import java.util.concurrent.SynchronousQueue;
 //线程池底层使用该结构
 //公平模式采用QUEUE，FIFO
 //非公平模式采用STACK，LIFO
-//当有线程将对象E放入队列里时，再有新的线程入队时，会先自旋一小段时间，然后UNPARK挂起(阻塞)
+//当有线程将对象E放入队列里时，再有新的线程PUT时，会先自旋一小段时间，然后PARK挂起(阻塞)
+//也就是说，不存在队列结构，前面的E不被消费，后续生产者只会被阻塞PARK
 //当有线程TAKE队列里的E时，取出E后，会唤醒后续等待的线程进队
+//当队列为空时，消费者线程TAKE时，会生产E对空的NODE节点，自旋一小段时间，如果没有匹配则挂起（阻塞）
+
+//由于无队列结构，没有“缓冲”，感觉会频繁的发生PARK，UNPARK，效率不高
 public class SynchronousQueNote {
 
-	public static void main(String[] args) {
-		SynchronousQueue sq;
-
+	public static void main(String[] args) throws InterruptedException {
+		SynchronousQueue<Integer> sq = new SynchronousQueue();
+		sq.put(1);
+		sq.put(1);
+		
 		//1.空队列，PUT，等待TAKE (e, false, 0)
         //空队列，TAKE，等待PUT
 		//2.非空队列，PUT。PUT被阻塞等待
