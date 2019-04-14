c018ddf61e42ab578ec65657ea38f7f8dc9ce59c
wabinogi <wabinogi@192.168.31.247>
2019-04-14 23:17:50 +0800

    21

--- BasicThread/src/ThreadPool/SingleThreadTest.java
+++ BasicThread/src/ThreadPool/SingleThreadTest.java
@@ -1,7 +1,9 @@
 package ThreadPool;
 
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 //单线程的线程池
 //底层采用1个大小的LinkedBlockingQueue
@@ -9,7 +11,9 @@ public class SingleThreadTest implements Runnable{
 
 	public static void main(String[] args) {
 
+	
          ExecutorService executor = Executors.newSingleThreadExecutor();
+         ScheduledExecutorService ses;
          SingleThreadTest stt  = new SingleThreadTest();
          for (int i = 0 ; i <=5 ; i++)
          {
